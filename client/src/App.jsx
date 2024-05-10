4c9cf97d90fd68b910b80dcd706c8f412760bafc
IT21181924 <IT21181924@my.sliit.lk>
2024-05-10 20:13:28 +0530

    Home, nav, footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import InstructorLayout from "./layouts/InstructorLayout";
 import AddCourse from "./pages/course/AddCourse";
 import Course from "./pages/course/Course";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Routes>
           <Route path="/" element={<AddCourse />} />
           <Route path="/course" element={<Course />} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          
         </Routes>
       </InstructorLayout>
     </BrowserRouter>
