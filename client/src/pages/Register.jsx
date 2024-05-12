0a0e9f270eec355f355cbc20c86c18ce01acc441
IT21181924 <IT21181924@my.sliit.lk>
2024-05-12 22:39:33 +0530

    wip

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -26,7 +26,7 @@ export default function Register() {
     };
 
     return (
-        <div className="flex justify-center items-center w-screen h-screen bg-white">
+        <div className="flex justify-center items-center w-screen h-100 bg-white">
             <div className="container mx-auto my-4 px-4 lg:px-20">
                 <div className="w-full p-8 my-4 md:px-12 lg:w-12/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
                     <h1 className="font-bold uppercase text-5xl">Join with Us</h1>
